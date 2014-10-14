5cdaf625f919c6838a0360a04277f02933740ddd
ervitis <vitomarti@gmail.com>
2014-10-14 02:02:22 +0200

    Installation setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from twipy import version
+from setuptools import setup
+
+setup(name='twipy',
+      version=version,
+      description='Another Twitter client using Python 2.7',
+      author='Victor Martin',
+      author_email='vitomarti@gmail.com',
+      url='http://github.com/ervitis/twipy',
+      keywords='twitter client console',
+      zip_safe=True,
+      entry_points={
+          'console_scripts': ['twipy = twipy.main:main']
+      })
