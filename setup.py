fa65f5e3c5ab0cbdd6d5246234b476a84a2f86c7
ervitis <vitomarti@gmail.com>
2014-10-15 20:22:10 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup
+import twipy
 
 setup(name='twipy',
-      version='0.0.1',
+      version=twipy.version,
+      packages=['twipy'],
       description='Another Twitter client using Python 2.7',
       author='Victor Martin',
       author_email='vitomarti@gmail.com',
@@ -13,6 +15,6 @@ setup(name='twipy',
       entry_points={
           'console_scripts': ['twipy = twipy.main:main']
       },
-      download_url='://github.com/ervitis/twipy/tarball/0.0.1',
+      download_url='://github.com/ervitis/twipy/tarball/' + twipy.version,
       license='MIT',
       classifiers=[])
