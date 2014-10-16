4cd6d251c8b0148aba6489b6d1042f293e31df68
ervitis <vitomarti@gmail.com>
2014-10-16 20:27:25 +0200

    Setup added requirements

--- setup.py
+++ setup.py
@@ -3,10 +3,19 @@
 from setuptools import setup
 import twipy
 
+REQUIREMENTS = ['oauth2']
+TEST_REQUIREMENTS = ['nose', 'coveralls', 'coverage']
+
+LONG_DESCRIPTION = """
+Twipy is a Twitter application for the Unix/Linux console and it's written in Python 2.7
+"""
+
+SHORT_DESCRIPTION = 'Another Twitter client using Python 2.7'
+
 setup(name='twipy',
       version=twipy.version,
       packages=['twipy'],
-      description='Another Twitter client using Python 2.7',
+      description=SHORT_DESCRIPTION,
       author='Victor Martin',
       author_email='vitomarti@gmail.com',
       url='https://github.com/ervitis/twipy',
@@ -17,4 +26,7 @@ setup(name='twipy',
       },
       download_url='https://github.com/ervitis/twipy/tarball/' + twipy.version,
       license='MIT',
+      install_requires=REQUIREMENTS,
+      tests_require=TEST_REQUIREMENTS,
+      long_description=LONG_DESCRIPTION,
       classifiers=[])
